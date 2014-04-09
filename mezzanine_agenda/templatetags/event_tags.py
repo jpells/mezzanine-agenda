740386303304ae39e816a405dddfd97091dab423
James Pells <jimmy@jamespells.com>
2014-04-09 09:05:13 -0400

    add a upcoming_events templatetag

--- mezzanine_agenda/templatetags/event_tags.py
+++ mezzanine_agenda/templatetags/event_tags.py
@@ -65,7 +65,7 @@ def event_authors(*args):
 @register.as_tag
 def recent_events(limit=5, tag=None, username=None, location=None):
     """
-    Put a list of recently published events into the template
+    Put a list of recent events into the template
     context. A tag title or slug, location title or slug or author's
     username can also be specified to filter the recent events returned.
 
@@ -78,6 +78,46 @@ def recent_events(limit=5, tag=None, username=None, location=None):
 
     """
     events = Event.objects.published().select_related("user")
+    events = events.filter(Q(start__lt=datetime.now()) | Q(end__lt=datetime.now()))
+    title_or_slug = lambda s: Q(title=s) | Q(slug=s)
+    if tag is not None:
+        try:
+            tag = Keyword.objects.get(title_or_slug(tag))
+            events = events.filter(keywords__keyword=tag)
+        except Keyword.DoesNotExist:
+            return []
+    if location is not None:
+        try:
+            location = EventLocation.objects.get(title_or_slug(location))
+            events = events.filter(location=location)
+        except EventLocation.DoesNotExist:
+            return []
+    if username is not None:
+        try:
+            author = User.objects.get(username=username)
+            events = events.filter(user=author)
+        except User.DoesNotExist:
+            return []
+    return list(events[:limit])
+
+
+@register.as_tag
+def upcoming_events(limit=5, tag=None, username=None, location=None):
+    """
+    Put a list of upcoming events into the template
+    context. A tag title or slug, location title or slug or author's
+    username can also be specified to filter the recent events returned.
+
+    Usage::
+
+        {% upcoming_events 5 as upcoming_events %}
+        {% upcoming limit=5 tag="django" as upcoming_events %}
+        {% upcoming_events limit=5 location="home" as upcoming_events %}
+        {% upcoming_events 5 username=admin as upcoming_events %}
+
+    """
+    events = Event.objects.published().select_related("user")
+    events = events.filter(Q(start__gt=datetime.now()) | Q(end__gt=datetime.now()))
     title_or_slug = lambda s: Q(title=s) | Q(slug=s)
     if tag is not None:
         try:
